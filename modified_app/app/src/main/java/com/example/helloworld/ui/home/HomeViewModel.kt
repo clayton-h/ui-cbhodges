24a56a26ad4efbe543126badbfb78075dd77527a
ClayT <claytonhodges326@gmail.com>
2024-02-21 08:39:30 -0700

    project_files

--- modified_app/app/src/main/java/com/example/helloworld/ui/home/HomeViewModel.kt
+++ modified_app/app/src/main/java/com/example/helloworld/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Clayton was here"
+        value = "Clayton was here."
     }
     val text: LiveData<String> = _text
 }
