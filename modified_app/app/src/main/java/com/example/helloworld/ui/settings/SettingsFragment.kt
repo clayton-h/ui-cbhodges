f02ffcc462f41a97b883bbe48b8e5c57a27e1f0b
ClayT <claytonhodges326@gmail.com>
2024-04-04 13:48:49 -0600

    refactoring

--- modified_app/app/src/main/java/com/example/helloworld/ui/settings/SettingsFragment.kt
+++ modified_app/app/src/main/java/com/example/helloworld/ui/settings/SettingsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.helloworld.databinding.FragmentNotificationsBinding
+import com.example.helloworld.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class SettingsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
