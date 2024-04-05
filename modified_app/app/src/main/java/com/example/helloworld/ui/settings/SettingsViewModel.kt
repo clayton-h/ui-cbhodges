c24e5c2f6f4766027ca05041b0752aacf05dd531
ClayT <claytonhodges326@gmail.com>
2024-04-04 20:46:35 -0600

    settings

--- modified_app/app/src/main/java/com/example/helloworld/ui/settings/SettingsViewModel.kt
+++ modified_app/app/src/main/java/com/example/helloworld/ui/settings/SettingsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "<<<settings under construction>>>"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
