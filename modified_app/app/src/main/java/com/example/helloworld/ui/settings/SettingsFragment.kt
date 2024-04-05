a344796371f7d44ba50ce339a922ee99cb573f16
ClayT <claytonhodges326@gmail.com>
2024-04-05 10:30:00 -0600

    settings

--- modified_app/app/src/main/java/com/example/helloworld/ui/settings/SettingsFragment.kt
+++ modified_app/app/src/main/java/com/example/helloworld/ui/settings/SettingsFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+//import androidx.lifecycle.ViewModelProvider
 import com.example.helloworld.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -22,17 +21,16 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
+//        val notificationsViewModel =
+//            ViewModelProvider(this)[SettingsViewModel::class.java]
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+//        val textView: TextView = binding.textNotifications
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return binding.root
     }
 
     override fun onDestroyView() {
